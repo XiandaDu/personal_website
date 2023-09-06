e467622831942985d6e95d4740ee6b3f5e0bcbf2
XiandaDu <283149519du@gmail.com>
2023-09-07 01:49:57 +0800

    feat: basic outline

---
+++ src/components/about.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="about">
+  	<div class="container">
+  	  <span class="tip">WHO AM I</span>
+  	  <h1 class="title">{{title}}</h1>
+  	  <span class="border one"></span>
+  	  <div class="a_info">
+  	  	<swiper :options="swiperOption" ref="mySwiper">
+  	      <!-- slides -->
+  	      <swiper-slide>
+  	        <p>Recruiting for a Backend or Frontend Developer??  You are in the right place!!!</p>
+  	      </swiper-slide>
+  	      <swiper-slide>
+  	        <p>I am good at Backend development using Spring Framework</p>
+  	      </swiper-slide>
+  	      <swiper-slide>
+            <p>Also I am a fan of Frontend development</p>
+  	      </swiper-slide>
+  	      <swiper-slide>
+            <p>I Am The One You Are Searching For!!</p>
+  	      </swiper-slide>
+  	      <div class="swiper-pagination" slot="pagination"></div>
+	        <div class="swiper-button-prev" slot="button-prev"></div>
+	        <div class="swiper-button-next" slot="button-next"></div>
+  	    </swiper>
+  	  </div>
+  	</div>
+  </div>
+</template>
+
+<script>
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+export default {
+  name: 'about',
+  data () {
+    return {
+      title: 'Self Introduction',
+      swiperOption: {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        }
+      }
+    }
+  },
+  components: {
+    swiper,
+    swiperSlide
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	.about{height: auto;width: 100%;margin: 0 auto;background-color: #f5f5f5;text-align: center;}
+  .container{height: auto;max-width: 1200px;margin: 0 auto;padding:4% 0;}
+  .tip{display:inline-block;position:relative;padding:6px 20px;margin:0 auto 10px;font-size: 20px;background:#03a9f4;color: #fff;}
+  .tip:after{position:absolute;width: 0;height: 0;left:42%;bottom:-10px;content: '';border-style: solid;border-width: 10px 10px 0 10px;border-color: #03a9f4 transparent;}
+  .title{padding: 6px 0 10px;font-family: 'Overlock', cursive;color: #2F2C06;font-size: 36px;}
+  .border{display:block;width: 20%;height: 1px;position:relative;margin:0 auto 30px;background: #8C8989;}
+  .border:after{position: absolute;top: 0;left: 30%;content: " ";width: 40%;height: 5px;margin-top:-2px;background: #03a9f4;}
+  .a_info{font-size: 20px;white-space: nowrap}
+  .swiper-container{width: 100%;height: 300px;line-height: 300px;overflow: hidden;text-align: center;}
+  .swiper-pagination{top:46%;}
+</style>
---
+++ src/components/skill.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="skill">
+  	<div class="container">
+  	  <span class="tip">WHAT CAN I DO</span>
+  	  <h1 class="title">{{title}}</h1>
+  	  <span class="border"></span>
+  	  <div class="s_wrapper">
+  	  	<div class="progress-bar" v-for="item in skill">
+  	      <progressBar :skill="item"></progressBar>
+  	    </div>
+  	  	<div class="clearfix"></div>
+  	  </div>
+  	</div>
+  </div>
+</template>
+
+<script>
+import progressBar from '@/components/progress-bar'
+export default {
+  name: 'skill',
+  data () {
+    return {
+    	title: 'My Technology Stack',
+    	skill: [
+    		{"type": "Java/Spring","percent": "95%"},
+    		{"type": "C++","percent": "85%"},
+    		{"type": "Python","percent": "75%"},
+    		{"type": "JavaScript","percent": "75%"},
+    		{"type": "Vue3","percent": "70%"},
+    		{"type": "MySQL/SQL","percent": "90%"},
+    		{"type": "Redis","percent": "85%"},
+        {"type": "MongoDB","percent": "65%"},
+        {"type": "Postman","percent": "95%"},
+        {"type": "Git","percent": "90%"},
+    		{"type": "Docker","percent": "80%"},
+    		{"type": "RocketMQ","percent": "50%"}
+    	]
+    }
+  },
+  components: {
+  	progressBar
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	.skill{height: auto;width: 100%;margin: 0 auto;background-color: #f5f5f5;text-align: center;}
+  .container{height: auto;max-width: 1200px;margin: 0 auto;padding:4% 0;}
+  .tip{display:inline-block;position:relative;padding:6px 20px;margin:0 auto 10px;font-size: 20px;background:#03a9f4;color: #fff;}
+  .tip:after{position:absolute;width: 0;height: 0;left:42%;bottom:-10px;content: '';border-style: solid;border-width: 10px 10px 0 10px;border-color: #03a9f4 transparent;}
+  .title{padding: 6px 0 10px;font-family: 'Overlock', cursive;color: #2F2C06;font-size: 36px;}
+  .border{display:block;width: 20%;height: 1px;position:relative;margin:0 auto 30px;background: #8C8989;}
+  .border:after{position: absolute;top: 0;left: 30%;content: " ";width: 40%;height: 5px;margin-top:-2px;background: #03a9f4;}
+
+	.progress-bar{float: left;width: 50%;height: auto;padding:0 40px;box-sizing: border-box;margin-top: 10px;}
+</style>
